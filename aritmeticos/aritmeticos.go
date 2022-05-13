a25df1b226d6a6cdb124491479312010945346a7
aeonmerx <103615875+AEON-MERX@users.noreply.github.com>
2022-05-13 03:43:54 -0500

    Add files via upload

---
+++ aritmeticos/aritmeticos.go
@@ -0,0 +1,19 @@
+package main
+import "fmt"
+func main()  {
+	a := 20
+	b := 10
+
+	//Suma
+	result := a+b 
+	fmt.Println("SUMA: ", result)
+	result = a-b 
+	fmt.Println("RESTA: ", result)
+	result = a*b 
+	fmt.Println("MULTI: ", result)
+	result = a/b 
+	fmt.Println("DIVISIÓN: ", result)
+
+  
+}
+
---
+++ variables/main.go
@@ -0,0 +1,16 @@
+package main
+import "fmt"
+func main()  {
+	var nombre1 string
+	nombre1 = "Andres"
+
+	var nombre2 string = "William"
+
+	edad := 26
+
+	fmt.Println(nombre1,nombre2,edad)
+
+
+
+
+}
